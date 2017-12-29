3ad0691c6d723480664457ee2b17da9e70f14e44
Cleber Santos <oclebersantos@gmail.com>
2017-12-29 16:18:51 -0200

    add dependencia para conexao do ionic no back

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Jenssegers\Mongodb\MongodbServiceProvider::class,
+        Barryvdh\Cors\ServiceProvider::class,
 
         /*
          * Package Service Providers...
