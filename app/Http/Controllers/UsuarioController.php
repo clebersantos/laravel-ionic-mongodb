3ef51fca0709a9b601d884c0b2f99a72e04ab71b
Cleber Santos <oclebersantos@gmail.com>
2017-12-30 14:20:48 -0200

    crud basico

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -71,7 +71,9 @@ class UsuarioController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        Usuario::find($id)->update($request->all());
+
+        return $request->all();
     }
 
     /**
@@ -82,6 +84,8 @@ class UsuarioController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Usuario::find($id)->delete();
+
+        return Usuario::all();
     }
 }
